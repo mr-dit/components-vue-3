6e90aef5d987339ba7590044d11661e219f9abf3
mr-dit <mezentsev000@mail.ru>
2023-09-05 16:20:45 +0500

    create component MyButton

---
+++ src/components/MyButton/MyButton.vue
@@ -0,0 +1,53 @@
+<template>
+    <button
+      class="button"
+      :class="{
+        'button--primary': type === 'primary',
+        'button--secondary': type === 'secondary',
+      }"
+      :style="{'borderRadius':`${borderRadius}`}"
+      :title="tooltip"
+      @click="handleClick"
+      :disabled="disabled"
+    >
+      <slot class="button__slot">{{ title }}</slot>
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      disabled: {
+        type: Boolean,
+        required: true
+      },
+      title: {
+        type: String,
+        required: false
+      },
+      tooltip: {
+        type: String,
+        required: false
+      },
+      type: {
+        type: String,
+        required: true,
+        validator: value => ['primary', 'secondary'].includes(value)
+      },
+      borderRadius: {
+        type: String,
+        required: false
+      }
+    },
+    methods: {
+      handleClick() {
+        if (!this.disabled) {
+          this.$emit('click', {});
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style scoped lang="scss" src="./MyButton.scss"></style>
+  
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,50 @@
+<script>
+import MyButton from '../components/MyButton';
+
+export default {
+    components: {
+        MyButton
+    },
+    methods: {
+        handleButtonClick(event) {
+            console.log(event);
+        }
+    }
+};
+</script>
+
+<template>
+    <div class="wrapper">
+        <div class="wrapper__item">
+            <my-button :disabled="false" title="Click" tooltip="This is a secondary button" type="secondary"
+                @click="handleButtonClick">
+            </my-button>
+
+            <my-button :disabled="false" title="Click Me" tooltip="This is a primary button" type="primary"
+                @click="handleButtonClick">
+            </my-button>
+
+            <my-button :disabled="true" tooltip="This is a secondary button" type="secondary" borderRadius="100%"
+                @click="handleButtonClick">
+                <i class="fa-solid fa-file"></i>
+            </my-button>
+
+        </div>
+        <div class="wrapper__item">
+
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+    display: flex;
+
+    &__item {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        align-items: start;
+    }
+}
+</style>
